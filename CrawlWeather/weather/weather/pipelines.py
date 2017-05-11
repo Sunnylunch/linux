cb5a6d4603752229d67e5ad3bcc9cae8e9c8d80e
root <2401503224@qq.com>
2017-05-11 07:21:32 -0700

    crawl all over the country weather

--- CrawlWeather/weather/weather/pipelines.py
+++ CrawlWeather/weather/weather/pipelines.py
@@ -3,13 +3,37 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/topics/item-pipeline.html
 
+#-*-coding:utf-8-*-
 import json
 import codecs
+import MySQLdb
+
 
 class WeatherPipeline(object):
 	def __init__(self):
 		self.file=codecs.open("air",mode="wb",encoding='utf-8')
+		self.conn=MySQLdb.Connection(host="localhost",user="root",passwd="xxxx",db="Weather",port=3306)
 	def process_item(self, item, spider):
-		line=json.dumps(dict(item))+'\n'
-		self.file.write(line.decode("unicode_escape"))
-        	return item
+		cur=self.conn.cursor()
+		i=0
+		while i < len(item['city']):
+			if item['city'] is None:
+				i=i+1
+			else:
+				line="""INSERT INTO weather VALUES ('"""
+				s=((item['city'])[i])[0:6]
+				line+=s
+				line+="','"
+				line+=((item['date'])[i])		
+				line+="','"
+				line+=((item['weather'])[i])		
+				line+="','"
+				line+=((item['air'])[i])		
+				line+="','"
+				line+=((item['winddir'])[i])		
+				line+="','"
+				line+=((item['windforce'])[i])	
+				line+="""')"""	
+				cur.execute(line.encode('utf-8'))
+				i=i+1
+		return item
--- CrawlWeather/weather/weather/spiders/weather.py
+++ CrawlWeather/weather/weather/spiders/weather.py
@@ -7,12 +7,34 @@ from ..items import WeatherItem
 class WeatherSpider(BaseSpider):
 	name='weather'
 	allowed_domains=['15tianqi.com']
-	start_urls=['http://www.15tianqi.com/shanxi/']
+	start_urls=['http://www.15tianqi.com']
 	def parse(self,response):
+		hxs=HtmlXPathSelector(response)
+		urls=hxs.select('//ul[1]/li/a/@href').extract()
+		
+		special_url=[
+				'http://www.15tianqi.com/shenzhen/',
+				'http://www.15tianqi.com/shanxixian/',
+				'http://www.15tianqi.com/hangzhou/',
+				'http://www.15tianqi.com/guangzhou/',
+				'http://www.15tianqi.com/beijing/',
+				'http://www.15tianqi.com/shanghai/',
+				'http://www.15tianqi.com/tianjin/',
+				'http://www.15tianqi.com/hongkong/',
+				'http://www.15tianqi.com/macao/'
+				]
+		for special_url in special_url:
+			yield Request(special_url,callback=self.parse_item)	
+	 	
+		for url in urls:
+			yield  Request(url,callback=self.parse_province)	
+
+	def parse_province(self,response):
 		hxs=HtmlXPathSelector(response)
 		urls=hxs.select('//div[@class="mcon center"]/a/@href').extract()
 	 	for url in urls:
-			yield Request(url,callback=self.parse_item)	
+			yield Request(url,callback=self.parse_item)			
+
 	def parse_item(self,response):
 		hxs=HtmlXPathSelector(response)
 		item=WeatherItem()
